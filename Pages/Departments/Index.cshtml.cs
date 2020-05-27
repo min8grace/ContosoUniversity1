a28ef19080148cb4ebe33ac3a5805fcf8820e0e6
min8grace <56316476+min8grace@users.noreply.github.com>
2020-05-26 22:32:30 -0700

    Concurrency

---
+++ Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity1.Data;
+using ContosoUniversity1.Models;
+
+namespace ContosoUniversity1.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity1.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity1.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+        }
+    }
+}
