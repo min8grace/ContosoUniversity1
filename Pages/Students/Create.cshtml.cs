3af679126567c81159c61c3ccdba8445ea2cc093
min8grace <56316476+min8grace@users.noreply.github.com>
2020-05-26 18:01:53 -0700

    CRUD

--- Pages/Students/Create.cshtml.cs
+++ Pages/Students/Create.cshtml.cs
@@ -31,15 +31,38 @@ namespace ContosoUniversity1.Pages.Students
         // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var emptyStudent = new Student();
+
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
             {
-                return Page();
+                _context.Students.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
 
-            _context.Students.Add(Student);
-            await _context.SaveChangesAsync();
+            return Page();
 
-            return RedirectToPage("./Index");
+            //TryUpdateModelAsync
+            //The preceding code creates a Student object and then uses posted form fields to update the Student object's properties. The TryUpdateModelAsync method:
+
+            //Uses the posted form values from the PageContext property in the PageModel.
+            //Updates only the properties listed(s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate).
+            //Looks for form fields with a "student" prefix.For example, Student.FirstMidName.It's not case sensitive.
+            //Uses the model binding system to convert form values from strings to the types in the Student model.For example, EnrollmentDate has to be converted to DateTime.
+
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            //_context.Students.Add(Student);
+            //await _context.SaveChangesAsync();
+
+            //return RedirectToPage("./Index");
         }
     }
 }
